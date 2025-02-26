3053ca36658536f27aedd2acf963174fab1a0f68
tuan24072002 <44106906+tuan24072002@users.noreply.github.com>
2025-02-26 17:26:58 +0700

    update

--- frontend/src/views/home/Home.tsx
+++ frontend/src/views/home/Home.tsx
@@ -5,11 +5,15 @@ import { useAppSelector } from "@/app/hooks";
 import FloatingLabelInput from "@/components/common/FloatingLabelInput";
 import { Button } from "@/components/ui/button";
 import { completed } from "@/utils/alert";
+import { useState } from "react";
+import { X } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label";
 
 const Home = () => {
     const navigate = useNavigate();
     const appState = useAppSelector(state => state.app);
-
+    const [showDialogPromotion, setShowDialogPromotion] = useState(true);
     return (
         <div className="h-[calc(100vh-86px)] w-screen overflow-hidden">
             <div className="h-full w-full relative">
@@ -37,36 +41,49 @@ const Home = () => {
                         ))
                     }
                 </Swiper>
-                <div className="absolute inset-0 bg-gradient-to-b from-black/10 to-highlight/10 backdrop-blur-[2px] z-10 lg:block hidden" />
-                <div className="absolute top-[100px] left-1/2 -translate-x-1/2 rounded-2xl shadow-highlight w-[90%] max-w-4xl z-20 lg:block hidden">
-                    <div className="bg-white/95 backdrop-blur-md shadow-2xl rounded-2xl p-4">
-                        <h2 className="text-2xl md:text-3xl font-semibold text-center mb-8 text-highlight">
-                            Đăng ký nhận thông tin ưu đãi
-                        </h2>
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                            <FloatingLabelInput
-                                label="Tên"
-                                className="bg-white/50 backdrop-blur-sm focus-within:bg-white transition-all duration-300"
-                            />
-                            <FloatingLabelInput
-                                label="Số điện thoại"
-                                className="bg-white/50 backdrop-blur-sm focus-within:bg-white transition-all duration-300"
-                            />
-                            <FloatingLabelInput
-                                label="Email"
-                                className="bg-white/50 backdrop-blur-sm focus-within:bg-white transition-all duration-300"
-                            />
+                {
+                    showDialogPromotion && <>
+                        <div className="absolute inset-0 bg-black/5 backdrop-blur-[1px] z-10 lg:block hidden" />
+                        <div className="absolute top-[100px] left-1/2 -translate-x-1/2 rounded-2xl w-[90%] max-w-4xl z-20 lg:block hidden">
+                            <div className="bg-white/95 backdrop-blur-md shadow-2xl rounded-2xl p-4 relative">
+                                <h2 className="text-2xl md:text-3xl font-semibold text-center mb-8 text-highlight">
+                                    Đăng ký nhận thông tin ưu đãi
+                                </h2>
+                                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                                    <FloatingLabelInput
+                                        label="Tên"
+                                        className="bg-white/50 backdrop-blur-sm focus-within:bg-white transition-all duration-300"
+                                    />
+                                    <FloatingLabelInput
+                                        label="Số điện thoại"
+                                        className="bg-white/50 backdrop-blur-sm focus-within:bg-white transition-all duration-300"
+                                    />
+                                    <FloatingLabelInput
+                                        label="Email"
+                                        className="bg-white/50 backdrop-blur-sm focus-within:bg-white transition-all duration-300"
+                                    />
+                                </div>
+                                <div className="flex justify-center mt-8">
+                                    <Button
+                                        onClick={() => completed('Cảm ơn bạn đã điền thông tin!')}
+                                        className="px-20 py-3 text-lg text-white bg-highlight hover:bg-highlight/90 transition-all duration-300 rounded-xl shadow-lg hover:shadow-xl transform hover:-translate-y-1"
+                                    >
+                                        Gửi
+                                    </Button>
+                                </div>
+                                <Button
+                                    onClick={() => setShowDialogPromotion(false)}
+                                    className="absolute top-2 right-2 bg-transparent hover:bg-transparent text-text hover:text-red-600 border border-black hover:border-red-600 !p-0 size-fit transition-all duration-300">
+                                    <X />
+                                </Button>
+                                <div>
+                                    <Checkbox />
+                                    <Label>Ẩn đi trong lần này</Label>
+                                </div>
+                            </div>
                         </div>
-                        <div className="flex justify-center mt-8">
-                            <Button
-                                onClick={() => completed('Cảm ơn bạn đã điền thông tin!')}
-                                className="px-20 py-3 text-lg text-white bg-highlight hover:bg-highlight/90 transition-all duration-300 rounded-xl shadow-lg hover:shadow-xl transform hover:-translate-y-1"
-                            >
-                                Gửi
-                            </Button>
-                        </div>
-                    </div>
-                </div>
+                    </>
+                }
             </div>
         </div>
     )
